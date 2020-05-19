7537b1373bf95d2d80fc2247a7f3e4745a20ab62
ashishchaudhary123 <ashishchaudhary123@users.noreply.github.com>
2020-05-19 12:55:42 -0400

    sample java doc

--- solution/src/main/java/ca/concordia/soen6461/abilities/FoodItem.java
+++ solution/src/main/java/ca/concordia/soen6461/abilities/FoodItem.java
@@ -2,6 +2,9 @@ package ca.concordia.soen6461.abilities;
 
 public class FoodItem implements IItem, SatchelMarker {
 
+	/**
+	 * ability level of an item
+	 */
 	private int abilityLevel;
 	
 	public FoodItem() {
