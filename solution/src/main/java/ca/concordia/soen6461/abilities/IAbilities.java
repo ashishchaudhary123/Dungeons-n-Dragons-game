7f4f5ecf82987b5e9670403769c4ac917341dd8c
[vibhaver] <[vibhaverma14.vv@gmail.com]>
2020-05-19 12:17:34 -0400

    sample javadoc

--- solution/src/main/java/ca/concordia/soen6461/abilities/IAbilities.java
+++ solution/src/main/java/ca/concordia/soen6461/abilities/IAbilities.java
@@ -2,5 +2,9 @@ package ca.concordia.soen6461.abilities;
 
 public interface IAbilities {
 
+	/**
+	 * Method is used to get ability for any item
+	 * @return ability value
+	 */
 	public int getAbility();
 }
